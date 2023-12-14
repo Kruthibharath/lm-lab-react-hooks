e06dc800e4984095314913b1b4967f66dda344ac
Kruthibharath <kruthi.bharath@gmail.com>
2023-12-14 13:16:08 +0000

    feat:implemented component render countu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import { Parent } from "./components/hooks/use_context/parent";
+/*import { Parent } from "./components/hooks/use_context/parent";
 import { APICall } from "./components/hooks/use_effect/use_effect";
 import { MemoExample } from "./components/hooks/use_memo/useMemo";
-import { Todo } from "./components/hooks/use_x/todo";
+import { Todo } from "./components/hooks/use_x/todo";*/
 import { CountRenders } from "./components/hooks/use_ref/count_renders";
 import { Focus } from "./components/hooks/use_ref/focus";
-import { CountCats } from "./components/hooks/use_state/count_cats";
+// import { CountCats } from "./components/hooks/use_state/count_cats";
 // import { TaskApp } from './components/hooks/use_reducer/todo_app';
 import { Section } from "./components/section";
 
@@ -23,18 +23,16 @@ function App() {
 
 				<Section>
 					<MemoExample />
-				</Section>
-
-				<Section>
-					<CountRenders />
-					<Focus/>
-				</Section>
-	*/}
+				</Section>*/}
         <Section>
+          <CountRenders />
+          <Focus />
+        </Section>
+        {/*<Section>
           <CountCats />
         </Section>
 
-        {/*<Section>
+        <Section>
 					<Todo />
 				</Section>
 
--- src/components/hooks/use_ref/count_renders.tsx
+++ src/components/hooks/use_ref/count_renders.tsx
@@ -1,21 +1,22 @@
-import { useState } from 'react';
+import { useRef, useState } from "react";
 
 export const CountRenders = () => {
-	const [value, setValue] = useState('');
-	const count = 1;
+  const [value, setValue] = useState("");
+  const renderCount = useRef(0);
+  const count = renderCount.current++;
 
-	return (
-		<>
-			<h2>useRef</h2>
-
-			<input
-				value={value}
-				type='text'
-				onChange={(e) => setValue(e.target.value)}
-			/>
-
-			<p>{value}</p>
-			<p>I have rendered {count} times</p>
-		</>
-	);
+  return (
+    <>
+      <h2>useRef</h2>
+      <input
+        value={value}
+        type="text"
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <p>{value}</p>
+      <p>I have rendered {count} times</p>
+    </>
+  );
 };
