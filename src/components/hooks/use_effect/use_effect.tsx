560b718305a2c654e3168aabed2dac5d20ee3376
Kruthibharath <kruthi.bharath@gmail.com>
2023-12-14 21:00:30 +0000

    feat:fetched data for Todo from API endpoint using useEffect

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,40 @@
 // make an api call using useEffect
 
-export const APICall = () => {
+import { useEffect, useState } from "react";
 
-    // Your code here!
-
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
 }
+
+export const APICall: React.FC = () => {
+  const [jsonData, setJsonData] = useState<Todo | null>(null);
+  useEffect(() => {
+    const fetchTodo = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      const data = await response.json();
+      setJsonData(data);
+      console.log(data);
+    };
+    fetchTodo();
+  }, []);
+
+  return (
+    <>
+      <h2>useEffect to fetch ToDo</h2>
+      {jsonData ? (
+        <div>
+          <h3>{jsonData.title}</h3>
+          <p>User ID: {jsonData.userId}</p>
+          <p>Completed: {jsonData.completed ? "Yes" : "No"}</p>
+        </div>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </>
+  );
+};
